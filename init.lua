c6af6750da89927df8638279e4c652c36bdb8254
rdhmdhl <reid@wearthefewture.com>
2025-02-14 11:35:38 -0800

    update init.lua

--- init.lua
+++ init.lua
@@ -287,6 +287,19 @@ require('lazy').setup({
     end,
   },
 
+  -- dashboard
+  {
+    'goolord/alpha-nvim',
+    -- dependencies = { 'echasnovski/mini.icons' },
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      local startify = require 'alpha.themes.startify'
+      -- available: devicons, mini, default is mini
+      -- if provider not loaded and enabled is true, it will try to use another provider
+      startify.file_icons.provider = 'devicons'
+      require('alpha').setup(startify.config)
+    end,
+  },
   -- colorscheme
   --If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
   {
